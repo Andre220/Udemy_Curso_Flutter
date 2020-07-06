61935ac2fbff63ed4ddcfccccefd7ae3e348ab22
andre220 <andreprofissional1997@gmail.com>
2020-07-05 23:29:36 -0300

    Adições referentes a aula 82 - layout da aplicação

--- buscador_gifs/lib/main.dart
+++ buscador_gifs/lib/main.dart
@@ -3,6 +3,15 @@ import 'package:buscador_gifs/UI/HomePage.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
-  ));
+      home: HomePage(),
+      theme: ThemeData(
+          hintColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            hintStyle: TextStyle(color: Colors.orange),
+          )
+      )
+  )
+  );
 }
